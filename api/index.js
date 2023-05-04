a62c1ff033a9ba73cec29b6eac61adb2a0f84135
Vi Minh Khôi <viminhkhoi2001@gmail.com>
2023-05-05 05:09:19 +0700

    b

--- api/index.js
+++ api/index.js
@@ -18,8 +18,22 @@ const readTextFile = (filename) => {
   return fs.readFileSync(filename, "utf8");
 };
 
-const headerContent = readTextFile("/header.txt");
-const footerContent = readTextFile("/footer.txt");
+const headerContent = `<?xml version='1.0' encoding='utf-8'?><?xml-stylesheet href="https://www.blogger.com/styles/atom.css" type="text/css"?>
+<feed xmlns='http://www.w3.org/2005/Atom' xmlns:openSearch='http://a9.com/-/spec/opensearchrss/1.0/' xmlns:gd='http://schemas.google.com/g/2005' xmlns:thr='http://purl.org/syndication/thread/1.0' xmlns:georss='http://www.georss.org/georss'>
+  <id>tag:blogger.com,1999:blog-1915332153780296371.archive</id>
+  <updated>2022-10-06T04:21:47.578-07:00</updated>
+  <title type='text'>Blogger Template</title>
+  <link rel='http://schemas.google.com/g/2005#feed' type='application/atom+xml' href='https://www.blogger.com/feeds/1915332153780296371/archive' />
+  <link rel='self' type='application/atom+xml' href='https://www.blogger.com/feeds/1915332153780296371/archive' />
+  <link rel='http://schemas.google.com/g/2005#post' type='application/atom+xml' href='https://www.blogger.com/feeds/1915332153780296371/archive' />
+  <link rel='alternate' type='text/html' href='http://fresh--blog-ht.blogspot.com/' />
+  <author>
+    <name>Blogger</name>
+    <email>noreply@blogger.com</email>
+    <gd:image rel='http://schemas.google.com/g/2005#thumbnail' width='32' height='32' src='//4.bp.blogspot.com/-tSozALL3D04/YRsu-CXaPgI/AAAAAAAAGH8/WoROPnh0LE85oDgzjWhOClriKDGGFn6YgCK4BGAYYCw/s32/unnamed.png' />
+  </author>
+  <generator version='7.00' uri='https://www.blogger.com'>Blogger</generator>`;
+const footerContent = "</feed>";
 
 const char2entity = {
   "'": "&#39;",
